b44ba45940e0da29efca3b5507e4de01d6c6ce80
Johan Ning <nijohang@amazon.com>
2026-07-20 10:39:45 -0400

    Remove duplicate plugin for completion list

--- nvim/lua/johan/plugins/lazydev.lua
+++ nvim/lua/johan/plugins/lazydev.lua
@@ -21,24 +21,5 @@ return {
             })
         end,
     },
-    { -- optional blink completion source for require statements and module annotations
-        "saghen/blink.cmp",
-        version = "1.*", -- uses a 1.x release that ships prebuilts
-        -- build = "cargo build --release",
-        opts = {
-            sources = {
-                -- add lazydev to your completion providers
-                default = { "lazydev", "lsp", "path", "snippets", "buffer" },
-                providers = {
-                    lazydev = {
-                        name = "LazyDev",
-                        module = "lazydev.integrations.blink",
-                        -- make lazydev completions top priority (see `:h blink.cmp`)
-                        score_offset = 100,
-                    },
-                },
-            },
-        },
-    },
     -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
